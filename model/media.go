a3c7e5820bed10a26cfa0581dabcb5abf6cfee19
Seif Wu <seif.wuxf@gmail.com>
2021-03-22 17:27:08 +0800

    fix: typo

--- model/media.go
+++ model/media.go
@@ -13,7 +13,7 @@ type Media struct {
 	Title        string         `json:"title" gorm:"comment:'标题'"`
 	Introduction string         `json:"introduction" gorm:"comment:'简介'"`
 	Region       string         `json:"region" gorm:"comment:'地区'"`
-	year         int64          `json:"year" gorm:"comment:'年份'"`
+	Year         int64          `json:"year" gorm:"comment:'年份'"`
 	Cover        string         `json:"cover" gorm:"comment:'封面'"`
 	Type         string         `json:"type" gorm:"comment:'类型'"`
 }
