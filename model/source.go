33e41a5315a6a8b4915843b038866be9b68d9574
Seif Wu <seif.wuxf@gmail.com>
2021-04-08 10:34:46 +0800

    feat: 来源表添加标识判断是否更新过

--- model/source.go
+++ model/source.go
@@ -5,10 +5,11 @@ import "time"
 type Source struct {
 	Base
 	Name          string    `json:"name" gorm:"comment:来源名称"`
-	OwnerID      int64     	`json:"ownerId" gorm:"comment:拥有者ID"`
+	OwnerID       int64     `json:"ownerId" gorm:"comment:拥有者ID"`
 	OwnerType     string    `json:"ownerType" gorm:"comment:拥有者表名"`
 	Domain        string    `json:"domain" gorm:"comment:采集网站"`
 	SourceUrl     string    `json:"sourceUrl" gorm:"comment:来源链接"`
+	UpdatedFlag   string    `json:"updatedFlag" gorm:"comment:来源是否更新标识"`
 	IsCompleted   bool      `json:"isCompleted" gorm:"comment:'是否完成'"`
 	LastCollectAt time.Time `json:"lastCollectAt" gorm:"comment:'最后采集时间'"`
 }
