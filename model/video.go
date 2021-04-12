070665921f18086bd0cc681eefaa7454609a9257
Seif Wu <seif.wuxf@gmail.com>
2021-04-12 23:47:05 +0800

    fix: 修改字段类型

--- model/video.go
+++ model/video.go
@@ -4,7 +4,7 @@ type Video struct {
 	Base
 	Title     string `json:"name" gorm:"comment:视频名称"`
 	Url       string `json:"url" gorm:"comment:视频地址"`
-	OrderSeq  string `json:"orderSeq" gorm:"comment:排序"`
+	OrderSeq  float64 `json:"orderSeq" gorm:"comment:排序"`
 	SourceID  string `json:"sourceId" gorm:"comment:来源ID"`
 	SourceUrl string `json:"sourceUrl" gorm:"comment:来源链接"`
 	Source    Source
